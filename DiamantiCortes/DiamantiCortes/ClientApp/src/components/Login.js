85dc0deb815f4b0d2434b0da534bfe5baae24149
davidmrtns <daviddmrtns@gmail.com>
2023-10-31 21:12:38 -0300

    Correções no estilo da tela de login

--- DiamantiCortes/DiamantiCortes/ClientApp/src/components/Login.js
+++ DiamantiCortes/DiamantiCortes/ClientApp/src/components/Login.js
@@ -1,19 +1,17 @@
 import style from "./Login.module.css";
 
-
-
-
-
 function Login() {
     return (
-    <div className={style.login}>
-        <h1>login</h1>
-        <input type="text" placeholder="nome"/>
-        <br/><br/>
-        <input type="password" placeholder="senha"/>
-        <br/><br/>
-        <button>entrar</button>
-    </div>
+        <div className={style.containerlogin}>
+            <div className={style.login}>
+                <h1>login</h1>
+                <input type="text" placeholder="nome" />
+                <br /><br />
+                <input type="password" placeholder="senha" />
+                <br /><br />
+                <button>entrar</button>
+            </div>
+        </div>
     );
 }
 
